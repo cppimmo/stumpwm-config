25a13129a2cd2db940e597d3a42f8ad814184989
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2022-09-17 00:26:14 -0500

    Change fonts; update .gitignore.

--- init.lisp
+++ init.lisp
@@ -3,10 +3,10 @@
 ;; My StumpWM configuration.
 
 ;; Specify how quicklisp should initialize.
-(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-									   (user-homedir-pathname))))
-  (when (probe-file quicklisp-init)
-	(load quicklisp-init)))
+;;(let ((quicklisp-init (merge-pathnames ".quicklisp/setup.lisp"
+;;									   (user-homedir-pathname))))
+;;  (when (probe-file quicklisp-init)
+;;	(load quicklisp-init)))
 
 (in-package :stumpwm)
 ;; Prevent having to prefix stumpwm: everytime I use a member of this package.
@@ -20,7 +20,7 @@
 (load "~/.stumpwm.d/cppimmo/colors.lisp")
 (load "~/.stumpwm.d/cppimmo/theme.lisp")
 (load "~/.stumpwm.d/cppimmo/placement.lisp")
-(load "~/.stumpwm.d/cppimmo/modeline.lisp")
+;; (load "~/.stumpwm.d/cppimmo/modeline.lisp")
 (load "~/.stumpwm.d/cppimmo/commands.lisp")
 (load "~/.stumpwm.d/cppimmo/keybinds.lisp")
 
@@ -31,13 +31,30 @@
 	  *message-window-gravity* :top-right
 	  *input-window-gravity* :top-left
 	  *input-history-ignore-duplicates* t
-	  *mouse-focus-policy* :sloppy ; Change window focus on hover.
+	  *mouse-focus-policy* :click ; Change window focus on click.
 	  *float-window-modifier* :SUPER
 	  *float-window-border* 3
 	  *float-window-title-height* 1
 	  *maxsize-border-width* 3
 	  *transient-border-width* 3
 	  *normal-border-width* 3) 
+
+(setf *window-format* "%m%n%s%c"
+	  *screen-mode-line-format* (list "[^B%n^b] %W^>%d")
+	  *time-modeline-string* "%d-%b-%Y %T"
+	  *mode-line-timeout* 2)
+(enable-mode-line (current-screen) (current-head) t)
+
+;;(ql:quickload "clx-truetype")
+;;(load-module "ttf-fonts")
+;;(xft:cache-fonts)
+;;(set-font (make-instance 'xft:font
+;;						 :family "Ubuntu Mono"
+;;						 :subfamily "Regular"
+;;						 :size 12
+;;						 :antialias t))
+ (set-font "-xos4-terminus-bold-r-normal-*-17-*-*-*-*-*-*-*")
+;; (set-font "-misc-ubuntu mono-bold-r-normal-*-17-*-*-*-*-*-*-*")
 
 ;; Set the prefix key.
 (set-prefix-key (kbd "s-SPC"))
@@ -48,4 +65,5 @@
 (run-shell-command "dunst")
 (run-shell-command "nm-applet")
 (run-shell-command "pasystray")
-
+;; Use feh --bg-scale ~/path/to/file beforehand.
+(run-shell-command "~/.fehbg")
